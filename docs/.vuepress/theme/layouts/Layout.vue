fb1b0c6f2e25172edad42203fcd9bcf1fa16930b
Raphi <raphael.antonietti@liip.ch>
2021-05-12 09:43:40 +0200

    add tailwind and different construct/öoading

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <h1 class="text-4xl text-red-600 p-12">hello world</h1>
+    <h1 class="btn">hello world</h1>
   </div>
 </template>
 <style lang="stylus">
-@import '../styles/theme.styl';
+@import '../../assets/css/main.postcss';
 </style>
