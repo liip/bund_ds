a4ed042f1a1d3f48f32eb9090793cf785b4029e7
rotwurstesser <raphael.antonitti@gmail.com>
2022-03-31 17:26:41 +0200

    Add vueselect and overstyle it

--- app/components/ch/components/Btn.vue
+++ app/components/ch/components/Btn.vue
@@ -1,101 +1,88 @@
 <template>
-  <component 
-    :is="tag" 
-    :href="link" 
-    :type="type" 
-    :class="classes" 
+  <component
+    :is="tag"
+    :href="link"
+    :type="type"
+    :class="classes"
     :aria-label="aria"
   >
-    <SvgIcon
-      v-if="this.icon"
-      :icon="this.icon"
-      class="btn__icon"
-    />
-    <span
-      v-if="this.label"
-      class="btn__text"
-    >
+    <SvgIcon v-if="this.icon" :icon="this.icon" class="btn__icon" />
+    <span v-if="this.label" class="btn__text">
       {{ label }}
     </span>
   </component>
 </template>
 
 <script>
-import SvgIcon from '../components/SvgIcon.vue';
+import SvgIcon from '../components/SvgIcon.vue'
 
 export default {
   name: 'btn',
   components: {
-    SvgIcon
+    SvgIcon,
   },
   props: {
     to: {
       type: String,
-      required: false
+      required: false,
     },
     variant: {
       type: String,
-      validator: (prop) => [
-        'outline',
-        'bare',
-        'filled',
-        'outline-negative',
-        'bare-negative',
-        'link',
-        'link-negative'
-      ].includes(prop)
+      validator: (prop) =>
+        [
+          'outline',
+          'bare',
+          'filled',
+          'outline-negative',
+          'bare-negative',
+          'link',
+          'link-negative',
+        ].includes(prop),
     },
     size: {
       type: String,
-      validator: (prop) => [
-        'sm',
-        'base',
-        'lg'
-      ].includes(prop)
+      validator: (prop) => ['sm', 'base', 'lg'].includes(prop),
     },
     icon: {
       type: String,
-      default: ''
+      default: '',
     },
     iconPos: {
       type: String,
-      validator: (prop) => [
-        'none',
-        'only',
-        'left',
-        'right',
-      ].includes(prop)
+      validator: (prop) => ['none', 'only', 'left', 'right'].includes(prop),
     },
     label: {
       type: String,
       required: true,
-    }
+    },
   },
 
   computed: {
-    tag () {
+    tag() {
       return this.to ? 'a' : 'button'
     },
 
-    type () {
+    type() {
       return this.to ? false : 'button'
     },
 
-    link () {
+    link() {
       return this.to ? this.to : false
     },
 
-    aria () {
-      return this.iconPos === 'only' && this.type === 'button' ? this.label : false
+    aria() {
+      return this.iconPos === 'only' && this.type === 'button'
+        ? this.label
+        : false
     },
-    
-    classes () {
+
+    classes() {
       let base = 'btn '
       if (this.variant) base += `btn--${this.variant} `
       if (this.size) base += `btn--${this.size} `
       if (this.iconPos) base += `btn--icon-${this.iconPos} `
       return base
     },
-  }
+  },
 }
 </script>
---
+++ app/components/ch/components/CustomSelect.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="form__group__select">
+    <v-select
+      :class="classes"
+      :disabled="disabled"
+      :clearable="clearable"
+      :placeholder="placeholder"
+      :searchable="searchable"
+      :options="options"
+      :closeOnSelect="false"
+    >
+      <template #open-indicator="{ attributes }">
+        <div class="v-select__icon">
+          <SvgIcon icon="ChevronDown" size="xl" />
+        </div>
+      </template>
+    </v-select>
+  </div>
+</template>
+
+<script>
+import vSelect from 'vue-select'
+import SvgIcon from './SvgIcon.vue'
+import 'vue-select/dist/vue-select.css'
+export default {
+  name: 'CustomSelect',
+  components: {
+    vSelect,
+    SvgIcon,
+  },
+
+  props: {
+    variant: {
+      type: String,
+      validator: (prop) => ['default', 'negative'].includes(prop),
+      default: 'default',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    label: {
+      type: String,
+    },
+    options: {
+      type: Array,
+    },
+    clearable: {
+      type: Boolean,
+      default: false,
+    },
+    searchable: {
+      type: Boolean,
+      default: false,
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
+  },
+
+  computed: {
+    classes() {
+      let base = 'custom-select '
+      if (this.variant) base += `custom-select--${this.variant} `
+      if (this.size) base += `custom-select--${this.size} `
+      if (this.iconPos) base += `custom-select--icon-${this.iconPos} `
+      return base
+    },
+  },
+}
+</script>
