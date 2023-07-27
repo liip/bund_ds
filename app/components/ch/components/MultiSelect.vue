1f289efb571dda47fb6e31c5cfc8c64cf730f14c
taazust5 <stefan.zurkinden@swisscom.com>
2023-07-27 17:03:53 +0200

    extended multiselect component

--- app/components/ch/components/MultiSelect.vue
+++ app/components/ch/components/MultiSelect.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="form__group__select">
-    <label v-if="label" :for="id" :class="labelClasses">
-      {{ label }}<span v-if="required" class="form__group__required" />
+    <label
+      v-if="label"
+      :for="getUniqueId('multi-select')"
+      :class="labelClasses"
+    >
+      {{ label }}
     </label>
     <div :class="selectWrapperClasses">
-      <vSelect
+      <v-select
+        :id="getUniqueId('multi-select')"
         :multiple="multiple"
         :placeholder="placeholder"
         :class="selectClasses"
         :disabled="disabled"
-        :components="{ Deselect, OpenIndicator }"
+        :components="{Deselect, OpenIndicator}"
         :options="options"
+        :name="name"
+        v-model="currentSelected"
+        :selectable="(option) => !excluded.includes(option) && (selectLimit ? !currentSelected || currentSelected.length < selectLimit : true)"
       />
       <div class="select__icon">
         <svg role="presentation" aria-hidden="true" viewBox="0 0 24 24">
@@ -31,96 +39,134 @@
 </template>
 
 <script>
-import vSelect from 'vue-select'
-
-export default {
-  name: 'MultiSelect',
-  components: {
+  const { v4: uuidv4 } = require('uuid');
+  import vSelect from 'vue-select'
+  export default {
+    name: 'MultiSelect',
+    components: {
     vSelect,
   },
-  data: () => ({
-    Deselect: {
-      render: (createElement) => createElement('span', '×'),
+    props: {
+      bare: {
+        type: Boolean,
+        default: false,
+      },
+      variant: {
+        type: String,
+        validator: (prop) => ['outline', 'negative'].includes(prop),
+        default: 'outline',
+      },
+      size: {
+        type: String,
+        validator: (prop) => ['sm', 'base', 'lg'].includes(prop),
+        default: 'base',
+      },
+      disabled: {
+        type: Boolean,
+        default: false,
+      },
+      hideLabel: {
+        type: Boolean,
+        default: false,
+      },
+      required: {
+        type: Boolean,
+        default: false,
+      },
+      label: {
+        type: String,
+      },
+      name: {
+        type: String,
+      },
+      message: {
+        type: String,
+      },
+      messageType: {
+        type: String,
+        validator: (prop) =>
+          ['error', 'warning', 'success', 'info'].includes(prop),
+      },
+      onChange: {
+        type: Function,
+        default: () => ({}),
+      },
+      options: {
+        type: Array,
+        default: () => [],
+      },
+      selected: {
+        type: Array,
+        default: () => [],
+      },
+      multiple: {
+        type: Boolean,
+        default: true,
+      },
+      placeholder: {
+        type: String,
+      },
+      excluded: {
+        type: Array,
+        default: () => [],
+      },
+      selectLimit: {
+        type: Number,
+      },
     },
-    OpenIndicator: {
-      render: (createElement) => createElement('span'), // hide the default icon
+    data() {
+      return {
+        selectId: '',
+        currentSelected: [],
+        Deselect: {
+          render: (createElement) => createElement('span', '×'),
+        },
+        OpenIndicator: {
+          render: (createElement) => createElement('span'),
+        },
+      };
     },
-  }),
-  props: {
-    options: {
-      type: Array,
-      default: () => [],
+    watch: {
+      // Emit on change
+      currentSelected() {
+        this.$emit('emitSelect', this.currentSelected);
+        this.onChange(this.currentSelected);
+      },
     },
-    variant: {
-      type: String,
-      validator: (prop) => ['outline', 'negative'].includes(prop),
-      default: 'outline',
+    created() {
+      // Set initial selected element
+      this.currentSelected = this.selected;
     },
-    size: {
-      type: String,
-      validator: (prop) => ['sm', 'base', 'lg'].includes(prop),
+    mounted() {
+      this.selectId = uuidv4();
     },
-    label: {
-      type: String,
+    methods: {
+      getUniqueId(text = '') {
+        return `${text}-${this.selectId}`;
+      },
     },
-    hideLabel: {
-      type: Boolean,
-      default: false,
+    computed: {
+      selectWrapperClasses() {
+        let base = 'select shadow-lg ';
+        if (this.bare) base += 'select--bare';
+        return base;
+      },
+      selectClasses() {
+        let base = '';
+        if (this.variant) base += `input--${this.variant} `;
+        if (this.size) base += `input--${this.size} `;
+        if (this.disabled) base += 'input--disabled ';
+        if (this.messageType) base += `input--${this.messageType} `;
+        return base;
+      },
+      labelClasses() {
+        let base = '';
+        if (this.variant === 'negative') base += 'text--negative ';
+        if (this.size) base += `text--${this.size} `;
+        if (this.hideLabel) base += 'sr-only ';
+        if (this.required) base += 'text--asterisk ';
+        return base;
+      },
     },
-    id: {
-      type: String,
-    },
-    name: {
-      type: String,
-    },
-    message: {
-      type: String,
-    },
-    messageType: {
-      type: String,
-      validator: (prop) =>
-        ['error', 'warning', 'success', 'info'].includes(prop),
-    },
-    required: {
-      type: Boolean,
-      default: false,
-    },
-    multiple: {
-      type: Boolean,
-      default: true,
-    },
-    placeholder: {
-      type: String,
-      default: '',
-    },
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
-  },
-
-  computed: {
-    selectWrapperClasses() {
-      let base = 'select shadow-lg '
-      if (this.bare) base += `select--bare `
-      return base
-    },
-    selectClasses() {
-      let base = ''
-      if (this.variant) base += `input--${this.variant} `
-      if (this.size) base += `input--${this.size} `
-      if (this.disabled) base += `input--disabled `
-      if (this.messageType) base += `input--${this.messageType} `
-      return base
-    },
-    labelClasses() {
-      let base = ''
-      if (this.variant === 'negative') base += `text--negative `
-      if (this.size) base += `text--${this.size} `
-      if (this.hideLabel) base += `sr-only `
-      if (this.required) base += `text--asterisk `
-      return base
-    },
-  },
-}
+  };
 </script>
